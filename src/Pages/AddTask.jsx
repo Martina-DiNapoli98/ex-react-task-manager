0722d88153376d510c1ca9d0f3d6a42bebd8f08e
Martina-DiNapoli98 <martinadinapoli1998@gmail.com>
2025-06-11 17:07:59 +0200

    Implement AddTask component with form for task creation

--- src/Pages/AddTask.jsx
+++ src/Pages/AddTask.jsx
@@ -1,7 +1,57 @@
+import { useState, useRef } from "react"
+
 export default function AddTask(){
+
+    const [title, setTitle] = useState("")
+    const statusRef = useRef("to Do")
+    const descriptionRef = useRef()
+
+
+    function handleSubmit(e){
+        e.preventDefault()
+
+        const newTask = {
+            title, 
+            description: descriptionRef.current.value,
+            status: statusRef.current.value
+        }
+        console.log("task creato", newTask)
+    }
+
     return(
         <>
-            <h1>This is add task</h1>
+        <div className="container">
+
+           <form onSubmit={handleSubmit}>
+            <input type="text"
+                className="form-control m-2"
+                name="title"
+                placeholder="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
+
+            <textarea
+                className="form-control m-2"
+                name="description"
+                placeholder="description"
+                ref={descriptionRef}
+            />
+
+            <select 
+            className="form-control m-2"
+            name="status" 
+            id="status" 
+            ref={statusRef}
+            >
+                <option value="To Do">To do</option>
+                <option value="Doing">Doing</option>
+                <option value="Done">Done</option>
+            </select>
+
+            <button type="submit">Invia</button>
+           </form>
+        </div>
         </>
     )
 }
