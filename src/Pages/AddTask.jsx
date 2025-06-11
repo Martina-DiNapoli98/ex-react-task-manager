dc4cd60e92b999a2dbe98686940c7dd4158ffd38
Martina-DiNapoli98 <martinadinapoli1998@gmail.com>
2025-06-11 17:23:35 +0200

    Add validation for task title in AddTask component

--- src/Pages/AddTask.jsx
+++ src/Pages/AddTask.jsx
@@ -5,10 +5,29 @@ export default function AddTask(){
     const [title, setTitle] = useState("")
     const statusRef = useRef("to Do")
     const descriptionRef = useRef()
+    
 
 
-    function handleSubmit(e){
+
+
+function titleIsValid(title) {
+  const symbols = "!@#$%^&*()-_=+[]{}|;:'\\,.<>?/`\"~";
+
+  if (title.trim() === "") {
+    alert("Devi inserire un titolo");
+    return false;
+  }
+
+  if (symbols.split("").some(s => title.includes(s))) {
+    alert("Il titolo contiene caratteri speciali non ammessi.");
+    return false;
+  }
+
+  return true;
+}
+        function handleSubmit(e){
         e.preventDefault()
+        if(!titleIsValid(title)) return
 
         const newTask = {
             title, 
@@ -17,7 +36,6 @@ export default function AddTask(){
         }
         console.log("task creato", newTask)
     }
-
     return(
         <>
         <div className="container">
