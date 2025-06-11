e7b7992a4aedf7192e9f83c230ab7818e51a4a88
Martina-DiNapoli98 <martinadinapoli1998@gmail.com>
2025-06-11 16:37:31 +0200

    Refactor TaskRow component to use useMemo for status color and formatted date

--- src/Components/TaskRow.jsx
+++ src/Components/TaskRow.jsx
@@ -1,17 +1,27 @@
+import { useMemo } from "react"
+
 export default function TaskRow({task}){
+
+    const colorStatus = useMemo(()=>{
+        switch(task.status){
+            case "To do" : return "red";
+            case "Doing" : return "yellow";
+            case "Done" : return "green";
+            default : return "black"
+        }
+    },[task.status])
+
+    const formattedDate = useMemo(()=>{
+        return new Date(task.createdAt).toLocaleDateString()
+    },[task.createdAt])
+
     return(
         <>
             <tr>
                 <td>{task.title}</td>
                 <td>{task.description}</td>
-                <td style={{color: task.status === "To do" ? "red" : 
-                    task.status === "Doing" ? "yellow" : 
-                    task.status === "Done" ? "green" : 
-                    "black"}}>{task.status}</td>
-                <td>{
-                        new Date(task.createdAt).toLocaleDateString()
-                    }
-                </td>
+                <td style={{color : colorStatus}}>{task.status}</td>
+                <td>{formattedDate}</td>
             </tr>
         </>
     )
